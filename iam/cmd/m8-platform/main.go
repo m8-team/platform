92b472908c619e369d0d00a4094477b09a0f02f2
Sergey Gorbachev <sergey.a.gorbachev@gmail.com>
2026-04-04 20:11:57 +0200

    Init

--- iam/cmd/m8-platform/main.go
+++ iam/cmd/m8-platform/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
 	"os/signal"
 	"syscall"
 
@@ -10,17 +14,37 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.New(os.Stderr, "", 0).Printf("m8-platform: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
 	cfg := config.Load()
 	application, err := app.New(ctx, cfg)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("build application: %w", err)
 	}
-	defer application.Close(context.Background())
 
-	if err := application.Serve(ctx); err != nil {
-		panic(err)
+	serveErr := application.Serve(ctx)
+	closeErr := application.Close(context.Background())
+
+	if serveErr != nil && closeErr != nil {
+		return errors.Join(
+			fmt.Errorf("serve application: %w", serveErr),
+			fmt.Errorf("close application: %w", closeErr),
+		)
 	}
+	if serveErr != nil {
+		return fmt.Errorf("serve application: %w", serveErr)
+	}
+	if closeErr != nil {
+		return fmt.Errorf("close application: %w", closeErr)
+	}
+
+	return nil
 }
