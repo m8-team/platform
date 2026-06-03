1535e0a81a212a8ef4a7f798b11a14650d0e09eb
Sergey Gorbachev <sergey.a.gorbachev@gmail.com>
2026-06-03 20:00:32 +0200

    Init

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/google/uuid v1.6.0
 	go.uber.org/automaxprocs v1.6.0
 	go.uber.org/fx v1.24.0
+	google.golang.org/grpc v1.80.0
 )
 
 require (
@@ -13,9 +14,13 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/mod v0.36.0 // indirect
+	golang.org/x/net v0.54.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.44.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
 	golang.org/x/tools v0.45.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260120221211-b8f7ae30c516 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
 
 replace github.com/m8-team/go-genproto => ./api/generate/go
--- internal/resourcemanager/module.go
+++ internal/resourcemanager/module.go
@@ -1,8 +1,13 @@
 package resourcemanager
 
 import (
+	"errors"
+	"strings"
+
 	"go.uber.org/fx"
 )
+
+var ErrEmptyServiceName = errors.New("resource manager service name is empty")
 
 type Config struct {
 	ServiceName string
@@ -11,6 +16,25 @@ type Config struct {
 
 func Module(cfg Config) fx.Option {
 	return fx.Module(
-		cfg.ServiceName,
+		"resourcemanager",
+		fx.Supply(cfg.normalized()),
+		fx.Invoke(configureModule),
 	)
+}
+
+func configureModule(cfg Config) error {
+	return cfg.Validate()
+}
+
+func (c Config) Validate() error {
+	if strings.TrimSpace(c.ServiceName) == "" {
+		return ErrEmptyServiceName
+	}
+
+	return nil
+}
+
+func (c Config) normalized() Config {
+	c.ServiceName = strings.TrimSpace(c.ServiceName)
+	return c
 }
