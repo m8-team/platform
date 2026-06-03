10e03f907addc83988da08704fa345b4be4250cb
Sergey Gorbachev <sergey.a.gorbachev@gmail.com>
2026-06-03 19:28:41 +0200

    Init

--- cmd/resource-manager/main.go
+++ cmd/resource-manager/main.go
@@ -1,7 +1,18 @@
 package main
 
-import _ "go.uber.org/automaxprocs"
+import (
+	"fmt"
+	"os"
+
+	_ "go.uber.org/automaxprocs"
+)
 
 func main() {
+	cfg, err := LoadConfig()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "load config: %v\n", err)
+		os.Exit(1)
+	}
 
+	NewApp(cfg).Run()
 }
--- internal/resourcemanager/module.go
+++ internal/resourcemanager/module.go
@@ -1,1 +1,16 @@
 package resourcemanager
+
+import (
+	"go.uber.org/fx"
+)
+
+type Config struct {
+	ServiceName string
+	Debug       bool
+}
+
+func Module(cfg Config) fx.Option {
+	return fx.Module(
+		cfg.ServiceName,
+	)
+}
