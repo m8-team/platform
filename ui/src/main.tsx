ba9821ed2cd54274a775fc3a43f657d054322513
Sergey Gorbachev <sergey.a.gorbachev@gmail.com>
2026-06-16 12:14:15 +0200

    Init

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,20 +1,13 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {configure, ThemeProvider} from '@gravity-ui/uikit';
 
 import '@gravity-ui/uikit/styles/fonts.css'
 import '@gravity-ui/uikit/styles/styles.css'
 
 import App from './App.tsx'
 
-configure({
-    lang: 'ru',
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ThemeProvider theme="light">
-          <App />
-      </ThemeProvider>
+    <App />
   </StrictMode>,
 )
