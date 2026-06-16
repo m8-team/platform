3b038cad33e71876e1f2837d6e9b69e52e323d54
Sergey Gorbachev <sergey.a.gorbachev@gmail.com>
2026-06-16 10:47:51 +0200

    Init

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,30 @@
-import {Button} from "@gravity-ui/uikit";
+import {Table, useTable} from '@gravity-ui/table'
+import type {ColumnDef} from '@gravity-ui/table/tanstack'
+
+interface Person {
+  id: string;
+  name: string;
+  age: number;
+}
+
+const columns: ColumnDef<Person>[] = [
+  {accessorKey: 'name', header: 'Name', size: 100},
+  {accessorKey: 'age', header: 'Age', size: 100},
+];
+
+const data: Person[] = [
+  {id: 'name', name: 'John', age: 23},
+  {id: 'age', name: 'Michael', age: 27},
+];
 
 function App() {
+  const table = useTable({
+    columns,
+    data,
+  });
+
   return (
-      <Button view="action" size="l" />
+    <Table table={table} />
   )
 }
 
