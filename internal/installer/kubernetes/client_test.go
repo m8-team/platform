ef8ce0c7c70ab8162ee02f2fda548e12fb6a74f9
Sergey Gorbachev <sergey.a.gorbachev@gmail.com>
2026-07-15 21:40:52 +0200

    Init

--- internal/installer/kubernetes/client_test.go
+++ internal/installer/kubernetes/client_test.go
@@ -4,6 +4,8 @@ import (
 	"regexp"
 	"testing"
 
+	installerv1alpha1 "github.com/m8platform/platform/api/installer/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -21,5 +23,39 @@ func TestInstallerCRDSingularNamesAreDNS1035Labels(t *testing.T) {
 		if !pattern.MatchString(singular) {
 			t.Fatalf("singular for %s = %q, want DNS-1035 label", crd.GetName(), singular)
 		}
+	}
+}
+
+func TestToUnstructuredAcceptsTypedValue(t *testing.T) {
+	release := installerv1alpha1.PlatformRelease{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: installerv1alpha1.GroupName + "/" + installerv1alpha1.Version,
+			Kind:       "PlatformRelease",
+		},
+		ObjectMeta: metav1.ObjectMeta{Name: "1.0.0"},
+		Spec: installerv1alpha1.PlatformReleaseSpec{
+			Kubernetes: installerv1alpha1.VersionRange{MinVersion: "1.30.0"},
+			Components: map[string]installerv1alpha1.ComponentRelease{
+				"platform": {
+					Version: "1.0.0",
+					Chart: installerv1alpha1.ArtifactRef{
+						Repository: "oci://registry.example.com/charts/platform",
+						Version:    "1.0.0",
+						Digest:     "sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+					},
+				},
+			},
+		},
+	}
+
+	object, err := toUnstructured(release)
+	if err != nil {
+		t.Fatalf("to unstructured: %v", err)
+	}
+	if object.GetKind() != "PlatformRelease" {
+		t.Fatalf("kind = %q, want PlatformRelease", object.GetKind())
+	}
+	if object.GetName() != "1.0.0" {
+		t.Fatalf("name = %q, want 1.0.0", object.GetName())
 	}
 }
